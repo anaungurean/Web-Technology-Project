f9f64e81d7740d3258adc90ceefd63715fb85006
anaungurean <anamariaungurean01@gmail.com>
2023-06-19 01:12:24 +0300

    -add-photo-database

--- FrontEnd/Scripts/AddPhoto.js
+++ FrontEnd/Scripts/AddPhoto.js
@@ -1,25 +1,21 @@
 function uploadFile() {
-    const fileInput = document.getElementById('photoInput');
-    const file = fileInput.files[0];
+  const fileInput = document.getElementById('photoInput');
+  const file = fileInput.files[0];
 
-    const formData = new FormData();
-    formData.append('photoInput', file);
+  const formData = new FormData();
+  formData.append('photo', file);
 
-    fetch('../../BackEnd/UploadPhoto.php', {
-      method: 'POST',
-      body: formData
-    })
-    .then(response => {
-      if (response.ok) {
-        console.log('File uploaded successfully.');
-        // Handle successful upload
-      } else {
-        console.error('Error uploading file.');
-        // Handle upload error
-      }
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      // Handle network or fetch error
-    });
+  fetch('http://localhost/TW/FrontEnd/UploadPhoto.php', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.json())
+  .then(data => {
+    // console.log('File uploaded successfully:', data);
+    // Optionally, display a success message to the user
+  })
+  .catch(error => {
+    // console.error('Error uploading file:', error);
+    // Optionally, display an error message to the user
+  });
 }
